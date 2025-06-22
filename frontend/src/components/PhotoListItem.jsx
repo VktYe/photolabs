910c443a0f5d077873b1b02c74050b606c1cafc6
VktYe <v.yehorenko@gmail.com>
2025-06-21 22:20:10 -0300

    Add styling to the PhotoListItem component using Sass

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,23 +1,21 @@
-import { Fragment } from "react";
 import "../styles/PhotoListItem.scss";
 
-
-
 const PhotoListItem = (props) => {
-  const {id, urls, user, location} = props.data;
-  console.log("PhotoListItem is loaded")
+  const { id, urls, user, location } = props.data;
+  console.log("PhotoListItem is loaded") // delete 
   /* Insert React */
   return (
-    <Fragment key={id}>
-      <div >
-        <img src={urls.regular}></img>
-      </div>
-      <div>
-        <img src={user.profile}></img>
-        <p>{user.name}</p>
-        <p>{location.city} {location.country}</p>
+    <>
+      <div key={id} className="photo-list__item">
+        <img className="photo-list__image" src={urls.regular}></img>
+        <div className="photo-list__user-details">
+          <img className="photo-list__user-profile" src={user.profile}></img>
+          <div className="photo-list__user-info">{user.name}
+            <div className="photo-list__user-location">{location.city} {location.country}</div>
+          </div>
         </div>
-    </Fragment>
+      </div>
+    </>
   )
 };
 
