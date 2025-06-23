4563f7c75be5f23d4adaa0320d870a5a6549d14e
VktYe <v.yehorenko@gmail.com>
2025-06-23 13:39:12 -0300

    Remove unused imports.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import './App.scss';
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
-import TopicListItem from './components/TopicListItem';
 import TopNavigation from './components/TopNavigationBar';
 
 
