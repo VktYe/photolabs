8a87acf5329ef67f0766a16b857d1f3caa87f7ea
VktYe <v.yehorenko@gmail.com>
2025-06-24 10:36:48 -0300

    Moved HomeRoute logic to HomeRoute component in the routes folder. File to delete.

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,21 +1,21 @@
-import PhotoList from "./PhotoList"
-import TopNavigation from "./TopNavigationBar"
+// import PhotoList from "./PhotoList"
+// import TopNavigation from "./TopNavigationBar"
 
-const HomeRoute = ({photos, topics, favourites, toggleFavourites}) => {
-  console.log("Photos:", photos) // delete
-  console.log("Topics:", topics) // delete
-  return (
-    <div className="home-route">
-      <TopNavigation 
-        topics={topics}
-        favourites={favourites}/>
-      <PhotoList 
-        photos={photos}
-        favourites={favourites}
-        toggleFavourites={toggleFavourites}
-        />
-    </div>
-  )
-}
+// const HomeRoute = ({photos, topics, favourites, toggleFavourites}) => {
+//   console.log("Photos:", photos) // delete
+//   console.log("Topics:", topics) // delete
+//   return (
+//     <div className="home-route">
+//       <TopNavigation 
+//         topics={topics}
+//         favourites={favourites}/>
+//       <PhotoList 
+//         photos={photos}
+//         favourites={favourites}
+//         toggleFavourites={toggleFavourites}
+//         />
+//     </div>
+//   )
+// }
 
-export default HomeRoute
+// export default HomeRoute
