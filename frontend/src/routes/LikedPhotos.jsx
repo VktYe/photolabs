1eceaf5818f05591e53038729122155916e47bc7
VktYe <v.yehorenko@gmail.com>
2025-06-30 16:00:03 -0300

    Starter for LikedPhotos Modal.

---
+++ frontend/src/routes/LikedPhotos.jsx
@@ -0,0 +1,16 @@
+
+// LikePhotos component - will display all liked photos by user 
+// - onClick => heart icon in nav bar
+// Opens Modal
+
+
+const LikedPhotosModal = () => {
+
+  return (
+    <div >
+      Liked Photos here!
+
+    </div>
+
+  )
+}
